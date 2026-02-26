e7b0b6ab13b766a577e48b876861a9ec13505232
Nafis Rahmani <nafisrahmani04@gmail.com>
2026-02-26 12:45:30 +0530

    Time: 5 ms (77.24%) | Memory: 45.9 MB (88.28%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        int original = x;
+        int reversed = 0;
+
+            if(x < 0){  //negative number
+                return false;
+            }
+           while(x != 0){
+            int digit = x % 10 ;
+            reversed = reversed * 10 + digit;
+            x = x / 10;
+           }
+          
+          return original == reversed;
+        
+    }
+}
