fd53f982da274948fac0740021f3060f1f989041
Nafis Rahmani <nafisrahmani04@gmail.com>
2026-04-24 12:00:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (99.23%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,26 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] == 0){
-                count0++;
-            }else if(nums[i] == 1){
-                count1++;
-            }else{
-              count2++;
-            }
-        }
+        //Optimal approach using  "dutch national flag algorithm"
 
-        for(int i = 0; i < count0; i++){
-            nums[i] = 0;
-        }
-        for(int i = count0; i<(count0+count1); i++){
-            nums[i] = 1;
-        }
-        for(int i = (count0+count1) ; i<nums.length; i++){
-            nums[i] =2;
-        }
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+        while(mid <= high){
+        if(nums[mid] == 0){
+           int temp = nums[mid];
+           nums[mid] = nums[low];
+           nums[low] = temp;
+            low++;
+            mid++;
+        } else if(nums[mid] == 1){
+            mid++;
+        } else{
+            int temp = nums[mid];
+            nums[mid] = nums[high];
+            nums[high] = temp;
+            high--;
+
+                 }
+             }
     }
 }
